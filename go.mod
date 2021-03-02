a03c846a3b38adc3b428fa06e2828346780e46c4
zjl <zhangjunli@01lb.com.cn>
2021-03-02 10:43:03 +0800

    【更新】添加rabbitmq

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/golang/protobuf v1.4.2
 	github.com/golang/snappy v0.0.2-0.20200707131729-196ae77b8a26 // indirect
 	github.com/jordan-wright/email v0.0.0-20200917010138-e1c00e156980
+	github.com/klauspost/compress v1.10.7 // indirect
 	github.com/minio/minio-go/v7 v7.0.3
 	github.com/onsi/ginkgo v1.14.0 // indirect
 	github.com/panjf2000/gnet v1.3.0
@@ -19,8 +20,8 @@ require (
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/viper v1.7.0
+	github.com/streadway/amqp v1.0.0
 	github.com/tsenart/vegeta/v12 v12.8.4
-	github.com/valyala/fasthttp v1.16.0
 	golang.org/x/net v0.0.0-20200822124328-c89045814202
 	golang.org/x/sys v0.0.0-20200824131525-c12d262b63d8 // indirect
 	golang.org/x/tools v0.0.0-20200117012304-6edc0a871e69
--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+//添加好友的方式
+const (
+	AddContactByGroup1 = iota + 1 //群内添加
+	AddContactByGroup             //群内添加
+
+	Weigh_Banker_Win     = 0x01
+	Weight_Player_Win    = 0x02
+	Weight_Draw          = 0x04
+	Weight_Banker_Double = 0x08
+	Weight_Player_Double = 0x10
+)
+
+func main() {
+	for {
+		time.Sleep(time.Second)
+		fmt.Println(time.Now().Second())
+	}
+}
+
+const (
+	RAND_NUM   = 0 // 纯数字
+	RAND_LOWER = 1 // 小写字母
+	RAND_UPPER = 2 // 大写字母
+	RAND_ALL   = 3 // 数字、大小写字母
+)
+
+var count int64
+
+//生成随机码
+func GenRandCode(inviteCodeLen int, targetKind int) string {
+	count++
+	ikind, kinds, result := targetKind, [][]int{[]int{10, 48}, []int{26, 97}, []int{26, 65}}, make([]byte, inviteCodeLen)
+	isAll := targetKind > 2 || targetKind < 0
+	rand.Seed(count)
+	for i := 0; i < inviteCodeLen; i++ {
+		if isAll {
+			ikind = rand.Intn(3)
+		}
+		scope, base := kinds[ikind][0], kinds[ikind][1]
+		result[i] = uint8(base + rand.Intn(scope))
+	}
+	return string(result)
 }
